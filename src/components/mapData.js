f7e65af9e1baa4f42ca21b709e7e61d0f2ca1a79
umarez <umar.izzuddin@ui.ac.id>
2021-03-02 20:28:43 +0700

    "TODO Done"

--- src/components/mapData.js
+++ src/components/mapData.js
@@ -2,26 +2,29 @@
 import React from "react";
 import { FaTrashAlt } from "react-icons/fa";
 import "twin.macro";
+import { IconContext } from "react-icons";
 
 export const MapData = ({ e, i, deleteHandler }) => {
   if (localStorage.getItem(e) !== null) {
     let dueDate = localStorage.getItem(e).split(" ").slice(0, 3).join(" ");
 
     return (
-      <div tw="m-5">
-        <h1>{e}</h1>
-        <h1 tw="flex items-center justify-between">
-          Due date : {dueDate}
-          <span
-            tw="flex items-center justify-center cursor-pointer hover:bg-gray-300 w-10 h-10"
-            onClick={() => deleteHandler(i)}
-          >
-            <FaTrashAlt />
-          </span>
-        </h1>
+      <div tw="m-5 p-6 border-2 bg-gray-700 border-gray-600 shadow-md rounded-lg flex justify-between items-center w-full font-poppins ">
+        <div tw="w-1/2">
+          <h1 tw="text-2xl font-medium text-white">{e}</h1>
+          <h1 tw="flex items-center justify-between text-sm text-gray-500 cursor-default">Due date : {dueDate}</h1>
+        </div>
+        <span
+          tw="flex items-center justify-center cursor-pointer w-10 h-10"
+          onClick={() => deleteHandler(i)}
+        >
+        <IconContext.Provider value={{className: 'react-icons'}}>
+          <FaTrashAlt />
+          </IconContext.Provider>
+        </span>
       </div>
     );
-  }else{
-    return null
+  } else {
+    return null;
   }
 };
